6731d5b54f2b7e4ec27f4aa4adcbe9c2b223aac9
CorAl <105968622+koreloop@users.noreply.github.com>
2022-12-27 22:07:32 +0800

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "000000",
+      "tmpSize": "10",
+      "cityColor": "000000",
+      "citySize": "10",
+      "aqiColor": "000000",
+      "aqiSize": "10",
+      "weatherIconSize": "10",
+      "alertIconSize": "10",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "city": "CN101190401",
+      "key": "ce8d8cc6ba5847b98f5b0f28e5e5c101"
+    }
+  }
